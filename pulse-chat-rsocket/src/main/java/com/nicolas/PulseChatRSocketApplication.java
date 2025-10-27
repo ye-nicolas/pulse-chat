8fd95cce9b9670991bbe57ddc50591021e75030d
nicolas.ye <nicolas.ye@neweb.com.tw>
2025-10-27 09:38:21 +0800

    feat: rsocket init

---
+++ pulse-chat-rsocket/src/main/java/com/nicolas/PulseChatRSocketApplication.java
@@ -0,0 +1,11 @@
+package com.nicolas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulseChatRSocketApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PulseChatRSocketApplication.class, args);
+    }
+}
