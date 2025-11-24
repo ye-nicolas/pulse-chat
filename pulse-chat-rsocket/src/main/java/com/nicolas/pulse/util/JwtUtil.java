47033e5d1dae64e70f6220a9b616d0d0833716a3
Nicolas YE <n6426527@gmail.com>
2025-11-24 22:55:14 +0800

    refactor: `JwtUtil`

--- pulse-chat-rsocket/src/main/java/com/nicolas/pulse/util/JwtUtil.java
+++ pulse-chat-rsocket/src/main/java/com/nicolas/pulse/util/JwtUtil.java
@@ -3,14 +3,20 @@ package com.nicolas.pulse.util;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.authentication.BadCredentialsException;
 
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
 import java.security.SecureRandom;
 import java.util.Base64;
 import java.util.Date;
+import java.util.Map;
 
 public class JwtUtil {
+    private static final String TYP = "typ";
+    private static final String REFRESH = "REFRESH";
+    private static final String ACCESS = "ACCESS";
+
     public static String generateHs256Key() {
         byte[] keyBytes = new byte[32];
         new SecureRandom().nextBytes(keyBytes);
@@ -21,18 +27,46 @@ public class JwtUtil {
         return Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
     }
 
-    public static String generateToken(SecretKey secretKey, String userName, Long expirationTime) {
+    private static String generateToken(SecretKey secretKey, String uuid, String accountId, Long expirationTimeMills, String type, Map<String, ?> map) {
         Date now = new Date();
         return Jwts.builder()
-                .subject(userName)
+                .header()
+                .type("JWT")
+                .and()
+                .id(uuid)
+                .subject(accountId)
                 .issuedAt(now)
-                .expiration(new Date(now.getTime() + expirationTime))
+                .notBefore(now)
+                .issuer("pulse-chat")
+                .expiration(new Date(now.getTime() + expirationTimeMills))
                 .signWith(secretKey, Jwts.SIG.HS256)
+                .claims(Map.of(TYP, type))
+                .claims(map)
                 .compact();
     }
 
-    public static Claims validateToken(SecretKey secretKey, String token) {
-        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload();
+    public static String generateRefreshToken(SecretKey secretKey, String uuid, String accountId, Long expirationTimeMills) {
+        return generateToken(secretKey, uuid, accountId, expirationTimeMills, REFRESH, Map.of());
+    }
+
+    public static String generateAccessToken(SecretKey secretKey, String uuid, String accountId, Long expirationTimeMills, Map<String, ?> map) {
+        return generateToken(secretKey, uuid, accountId, expirationTimeMills, ACCESS, map);
+    }
+
+    private static Claims validateAccessToken(SecretKey secretKey, String token) {
+        return validateToken(secretKey, token, ACCESS);
+    }
+
+    private static Claims validateRefreshToken(SecretKey secretKey, String token) {
+        return validateToken(secretKey, token, REFRESH);
+    }
+
+    private static Claims validateToken(SecretKey secretKey, String token, String type) {
+        Claims payload = Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload();
+        if (payload.get(TYP, String.class).equals(type)) {
+            throw new BadCredentialsException("Token type '%S' is not allowed here.".formatted(type));
+        }
+        return payload;
     }
 
 }
