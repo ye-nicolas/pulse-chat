d372d2be6e157b16d01e3066acf09eac2eeb6b7c
Nicolas YE <n6426527@gmail.com>
2025-11-30 19:06:38 +0800

    add: `CreateFriendShipUseCase` & `UpdateFriendShipStatusToAcceptedUseCase`

---
+++ pulse-chat-rsocket/src/main/java/com/nicolas/pulse/adapter/dto/req/CreateFriendShipReq.java
@@ -0,0 +1,15 @@
+package com.nicolas.pulse.adapter.dto.req;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateFriendShipReq {
+    private String requesterAccountId;
+    private String recipientAccountId;
+}
