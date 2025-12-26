96cb8893d096e77e5b10760e7f14572f3048b183
Nicolas YE <n6426527@gmail.com>
2025-12-26 22:55:18 +0800

    add: create sql & api doc

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # PulseChat
 High QPS Stand-alone chat system
+# API
+## Account
+| Url                   | Method | 功能            |
+| --------------------- | ------ | --------------- |
+| /accounts/            | Get    | 查找全部        |
+| /accounts/{accountId} | Get    | 查找AccountById |
+
+## Role
+| Url             | Method | 功能         |
+| --------------- | ------ | ------------ |
+| /roles/         | Get    | 查找全部     |
+| /roles/{roleId} | Get    | 查找RoleById |
+| /roles/         | Post   | 建立Create   |
+
+## Auth
+| Url           | Method | 功能        |
+| ------------- | ------ | ----------- |
+| /auth/login   | Post   | 登陸        |
+| /auth/refresh | Post   | 更新Token   |
+| /auth/account | Post   | 建立Account |
+
+## Friend Ship
+| Url                          | Method | 功能                      |
+| ---------------------------- | ------ | ------------------------- |
+| /friend-ships/               | Get    | 獲取Account對應的好友清單 |
+| /friend-ships/               | Post   | 添加好友                  |
+| /friend-ships/{friendShipId} | Patch  | 確認交友邀請              |
+
+## Chat Room
+| Url                         | Method | 功能             |
+| --------------------------- | ------ | ---------------- |
+| /chat-rooms/                | Get    | 查詢所有聊天室   |
+| /chat-rooms/{roomId}        | Get    | 根據ID查詢聊天室 |
+| /chat-rooms/                | Post   | 創建聊天室       |
+| /chat-rooms/{roomId}/member | Post   | 添加用戶         |
+| /chat-rooms/{roomId}/member | Delete | 移除用戶         |
+| /chat-rooms/{roomId}        | Delete | 刪除聊天室       |
+
+# WebSocket
+| endpoint                        | 功能                 |
+| ------------------------------- | -------------------- |
+| session.open.room.{roomId}      | 根據房間ID建立連線   |
+| chat.message.add                | 發送訊息             |
+| chat.message.update.{messageId} | 更新信息             |
+| chat.message.delete.{messageId} | 刪除信息             |
+| chat.message.read.{messageId}   | 讀取信息             |
+| chat.history.get.{roomId}       | 獲取該聊天室歷史資訊 |
--- docker/postgresql/init/init.sql
+++ docker/postgresql/init/init.sql
@@ -1,1 +1,121 @@
 CREATE SCHEMA IF NOT EXISTS pulse_chat
+
+-- account
+CREATE TABLE pulse_chat.account (
+	id bpchar(26) NOT NULL,
+	"name" varchar NOT NULL,
+	show_name varchar NOT NULL,
+	"password" varchar NOT NULL,
+	is_active bool NOT NULL,
+	last_login_at timestamptz NULL,
+	created_by bpchar(26) NOT NULL,
+	updated_by bpchar(26) NOT NULL,
+	created_at timestamptz NOT NULL DEFAULT now(),
+	updated_at timestamptz NOT NULL DEFAULT now(),
+	remark text NULL,
+	CONSTRAINT user_pk PRIMARY KEY (id),
+	CONSTRAINT user_uk UNIQUE ("name")
+);
+
+-- role
+CREATE TABLE pulse_chat."role" (
+	id bpchar(26) NOT NULL,
+	"name" varchar NOT NULL,
+	created_by bpchar(26) NOT NULL,
+	updated_by bpchar(26) NOT NULL,
+	created_at timestamptz NOT NULL DEFAULT now(),
+	updated_at timestamptz NOT NULL DEFAULT now(),
+	remark text NULL,
+	CONSTRAINT role_pk PRIMARY KEY (id),
+	CONSTRAINT role_uk UNIQUE (name)
+);
+
+-- role_privilege
+CREATE TABLE pulse_chat.role_privilege (
+	role_id bpchar(26) NOT NULL,
+	privilege varchar NOT NULL,
+	created_by bpchar(26) NOT NULL,
+	created_at timestamptz NOT NULL DEFAULT now(),
+	id bpchar(26) NOT NULL
+);
+
+-- account_role
+CREATE TABLE pulse_chat.account_role (
+	id bpchar(26) NOT NULL,
+	account_id bpchar(26) NOT NULL,
+	role_id bpchar(26) NOT NULL,
+	created_by bpchar(26) NOT NULL,
+	created_at timestamptz NOT NULL DEFAULT now(),
+	CONSTRAINT account_role_pk PRIMARY KEY (id),
+	CONSTRAINT account_role_uk UNIQUE (account_id, role_id)
+);
+
+-- friend_ship
+CREATE TABLE pulse_chat.friend_ship (
+	id bpchar(26) NOT NULL,
+	requester_account_id bpchar(26) NOT NULL,
+	recipient_account_id bpchar(26) NOT NULL,
+	"status" varchar NOT NULL DEFAULT 'PENDING'::character varying,
+	created_by bpchar(26) NOT NULL,
+	updated_by bpchar(26) NOT NULL,
+	created_at timestamptz NOT NULL DEFAULT now(),
+	updated_at timestamptz NOT NULL DEFAULT now(),
+	CONSTRAINT friend_ship_pk PRIMARY KEY (id),
+	CONSTRAINT friend_ship_uk_1 UNIQUE (requester_account_id, recipient_account_id),
+	CONSTRAINT friend_ship_uk_2 UNIQUE (recipient_account_id, requester_account_id)
+);
+
+-- chat_room
+CREATE TABLE pulse_chat.chat_room (
+	id bpchar(26) NOT NULL,
+	"name" varchar NOT NULL,
+	created_by bpchar(26) NOT NULL,
+	updated_by bpchar(26) NOT NULL,
+	created_at timestamptz NOT NULL DEFAULT now(),
+	updated_at timestamptz NOT NULL DEFAULT now(),
+	CONSTRAINT chat_room_pk PRIMARY KEY (id)
+);
+
+-- chat_room_member
+CREATE TABLE pulse_chat.chat_room_member (
+	id bpchar(26) NOT NULL,
+	account_id bpchar(26) NOT NULL,
+	room_id bpchar(26) NOT NULL,
+	created_by bpchar(26) NOT NULL,
+	updated_by bpchar(26) NOT NULL,
+	created_at timestamptz NOT NULL DEFAULT now(),
+	is_muted bool NOT NULL DEFAULT false,
+	is_pinned bool NOT NULL DEFAULT false,
+	updated_at timestamptz NOT NULL DEFAULT now(),
+	CONSTRAINT chat_room_member_pk PRIMARY KEY (id),
+	CONSTRAINT chat_room_member_uk UNIQUE (account_id, room_id)
+);
+
+-- chat_message
+CREATE TABLE pulse_chat.chat_message (
+	id bpchar(26) NOT NULL,
+	room_id bpchar(26) NOT NULL,
+	member_id bpchar(26) NOT NULL,
+	"type" varchar NOT NULL DEFAULT 'TEXT'::character varying,
+	"content" text NOT NULL,
+	is_delete bool NOT NULL DEFAULT false,
+	created_by bpchar(26) NOT NULL,
+	updated_by bpchar(26) NOT NULL,
+	created_at timestamptz NOT NULL DEFAULT now(),
+	is_muted bool NOT NULL DEFAULT false,
+	is_pinned bool NOT NULL DEFAULT false,
+	CONSTRAINT chat_message_pk PRIMARY KEY (id)
+);
+
+-- chat_message_last_read
+CREATE TABLE pulse_chat.chat_message_last_read (
+	id bpchar(26) NOT NULL,
+	last_message_id bpchar(26) NOT NULL,
+	room_id bpchar(26) NOT NULL,
+	member_id bpchar(26) NOT NULL,
+	created_by bpchar(26) NOT NULL,
+	created_at timestamptz NOT NULL DEFAULT now(),
+	updated_at timestamptz NOT NULL DEFAULT now(),
+	CONSTRAINT chat_message_last_read_pk PRIMARY KEY (id),
+	CONSTRAINT chat_message_last_read_un UNIQUE (last_message_id, room_id, member_id)
+);
